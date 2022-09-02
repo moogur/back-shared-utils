61851a8fe7f7a733dea2b059b4fab0e90a331ddc
Alexey <moogur-mail@yandex.ru>
2022-09-02 20:21:39 +0300

    [UTL-152] add build (tsc)

--- lib/const/date.d.ts
+++ lib/const/date.d.ts
@@ -2,4 +2,3 @@ export declare const minDateIso = "1910-01-01T00:00:00.000Z";
 export declare const minDateTimestamp = -1893456000;
 export declare const maxDateIso = "2035-01-01T00:00:00.000Z";
 export declare const maxDateTimestamp = 2051222400;
-export declare const maxDateTimestamp2 = 2051222400;

07e57bc117a386422062805e1e19c8b75a39a8a5
Alexey <moogur-mail@yandex.ru>
2022-09-02 20:16:11 +0300

    [UTL-152] add build (tsc)

--- src/const/date.ts
+++ src/const/date.ts
@@ -3,3 +3,4 @@ export const minDateTimestamp = -1_893_456_000;
 
 export const maxDateIso = '2035-01-01T00:00:00.000Z';
 export const maxDateTimestamp = 2_051_222_400;
+export const maxDateTimestamp2 = 2_051_222_400;
